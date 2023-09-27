b9cb3aadb53a0d2f58da79fc106828389b5bda09
Gaganpreet Kaur <kaurgagan951@gmail.com>
2023-09-26 22:36:06 -0400

    First Commit

---
+++ src/test/java/module1/TestScenario1.java
@@ -0,0 +1,25 @@
+package module1;
+
+import org.testng.annotations.Test;
+
+public class TestScenario1 {
+	
+	@Test
+	public void testcase01() {
+		
+		System.out.println("I am module 1 test scenario 1 test case 01");
+	}
+	
+	@Test 
+	public void testcase02() {
+		
+		System.out.println("I am module 1 test scenario 1 test case 02");
+	}
+	
+	@Test
+	public void testcase03() {
+	
+	System.out.println("I am module 1 test scenario 1 test case 03");
+}
+
+}
---
+++ src/test/java/module1/TestScenario2.java
@@ -0,0 +1,25 @@
+package module1;
+
+import org.testng.annotations.Test;
+
+public class TestScenario2 {
+	
+	@Test
+	public void testcase01() {
+		
+		System.out.println("I am module 1 test scenario 2 test case 01");
+	}
+	
+	@Test 
+	public void testcase02() {
+		
+		System.out.println("I am module 1 test scenario 2 test case 02");
+	}
+	
+	@Test
+	public void testcase03() {
+	
+	System.out.println("I am module 1 test scenario 2 test case 03");
+}
+
+}
